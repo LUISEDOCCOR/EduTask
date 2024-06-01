bbf2d30b23e755e195648453ad87f469540da1eb
Luis Eduardo Ocegueda Cortes <luiseduardoocegueda@gmail.com>
2024-06-01 15:46:53 -0600

    fix: size button nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -70,7 +70,7 @@ export const Nav = () => {
                             strokeWidth="2"
                             strokeLinecap="round"
                             strokeLinejoin="round"
-                            className="h-6 w-6">
+                            className="h-8 w-8">
                             <path
                                 stroke="none"
                                 d="M0 0h24v24H0z"

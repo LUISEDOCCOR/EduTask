a178a872193e1e89b4d24747daef518703f36be2
Luis Eduardo Ocegueda Cortes <luiseduardoocegueda@gmail.com>
2024-05-31 20:30:27 -0600

    feat: landing page, sections need to be made responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				cWhite: "#f1f1f1",
+				cGrey: "#7F7F7F",
+				cBlack: "#151515"
+			}
+		},
 	},
 	plugins: [],
 }
